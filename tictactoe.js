63bcd0a7ec1f81c3c15cf9b063df00b31a7cbfe0
ChristopherStanley14 <christopherstanley@ChristopherStanleyMBP.local>
2016-09-29 12:59:30 -0400

    Update navbar

---
+++ tictactoe.js
@@ -0,0 +1,53 @@
+// Setup Game
+
+
+$(function() {
+    $("#btnX1").hide();
+    $("#btnO1").hide();
+    $("#btn1").show();
+   
+    $("#btnX2").hide();
+    $("#btnO2").hide();
+    $("#btn2").show();
+   
+    $("#btnX3").hide();
+    $("#btnO3").hide();
+    $("#btn3").show();
+   
+    $("#btnX4").hide();
+    $("#btnO4").hide();
+    $("#btn4").show();
+   
+    $("#btnX5").hide();
+    $("#btnO5").hide();
+    $("#btn5").show();
+   
+    $("#btnX6").hide();
+    $("#btnO6").hide();
+    $("#btn6").show();
+  
+    $("#btnX7").hide();
+    $("#btnO7").hide();
+    $("#btn7").show();
+   
+    $("#btnX8").hide();
+    $("#btnO8").hide();
+    $("#btn8").show();
+    
+    $("#btnX9").hide();
+    $("#btnO9").hide();
+    $("#btn9").show();
+
+    startUp();
+});
+
+
+
+
+
+function player2() {
+   
+};
+
+
+
